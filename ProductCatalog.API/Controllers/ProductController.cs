b1678c080f67de550c6e347b25371d739896b434
Aneena Shaji <ashaji@suyati.com>
2022-03-31 15:18:51 +0530

    comment

---
+++ ProductCatalog.API/Controllers/ProductController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ProductCatalog.API.Models;
+using ProductCatalog.API.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProductCatalog.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<Product> Get()
+        {
+            ProductModel pr = new ProductModel();
+            return pr.findAll();
+        }
+    }
+}
---
+++ ProductCatalog.API/ViewModel/ProductModel.cs
@@ -0,0 +1,43 @@
+﻿using ProductCatalog.API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProductCatalog.API.ViewModel
+{
+    public class ProductModel
+    {
+        private Product prod;
+        //sucess
+        public List<Product> _products { get; set; }
+        public List<Product> findAll()
+        {
+            _products = new List<Product>
+            {
+                new Product()
+                {
+                    Id ="1",
+                    Name="SAMSUNG",
+                    price =200
+
+                },
+                new Product()
+                {
+                    Id ="2",
+                    Name="nokia",
+                    price =250
+
+                }
+            };
+            return _products;
+        }
+        public Product find(string id)
+        {
+            List<Product> products = findAll();
+            var prod = products.Where(a => a.Id == id).FirstOrDefault();
+            return prod;
+        }
+    }
+}
+
